eb5cd1b14d54b6d778262efadcda216b2d5859ae
David Greisen <dgreisen@openlawlib.org>
2018-08-18 12:02:09 -0400

    xml fixes

--- server.py
+++ server.py
@@ -12,10 +12,21 @@ from http.server import SimpleHTTPRequestHandler, HTTPServer
 import argparse
 import sys, os
 import urllib.parse
+import urllib.request
+import http.client
 import posixpath
 import json
 
+
 DIR = os.path.abspath(os.path.dirname(__file__))
+
+with open('version.json') as f:
+    VERSION_INFO = json.load(f)
+DEFAULT_SEARCH_URL = VERSION_INFO['meta']['canonical-urls']['html']
+PORTAL_CLIENT_CLASS = None
+PORTAL_HOST = None
+SEARCH_CLIENT_CLASS = None
+SEARCH_HOST = None
 
 filetypes = {
     'html',
@@ -35,8 +46,55 @@ filetypes = {
     'woff',
 }
 
+
 class RequestHandler(SimpleHTTPRequestHandler):
+    def _proxy(self, Client, host, upstream_name):
+        """
+        proxy the current request to the given host using the given
+        http.client Client class. 404 if not configured to proxy
+        the path.
+        """
+        if Client is None:
+            self.send_response(400)
+            self.end_headers()
+            message = 'Local server not configured to proxy {0}. Please run with the "--{0}-proxy-url" flag '.format(upstream_name)
+            self.wfile.write(message.encode('utf-8'))
+            return
+        client = Client(host)
+        req_headers = {}
+        req_headers.update(self.headers)
+        del req_headers['Host']
+        req_headers.update({
+            'X-Forwarded-For': 'self.address_string()',
+            'X-Forwarded-Host': self.headers['Host'],
+            'X-Forwarded-Proto': 'http',
+        })
+        client.request('GET', self.path, headers=req_headers)
+        try:
+            resp = client.getresponse()
+        except:
+            self.send_response(500)
+            self.end_headers()
+            host = client.host
+            scheme = type(client).__name__[:-10].lower()
+            url = urllib.parse.urlunsplit((host, scheme, '', ''))
+            message = 'Something went wrong proxying to {}'.format(url)
+            self.wfile.write(message.encode('utf-8'))
+        else:
+            self.send_response(resp.code)
+            for k, v in resp.headers.items():
+                if k not in ('Transfer-Encoding', 'Connection'):
+                    self.send_header(k, v)
+            self.end_headers()
+            self.copyfile(resp, self.wfile)
+
     def do_GET(self):
+        if self.path.startswith('/_portal'):
+            return self._proxy(PORTAL_CLIENT_CLASS, PORTAL_HOST, 'portal')
+
+        if self.path.startswith('/_search'):
+            return self._proxy(SEARCH_CLIENT_CLASS, SEARCH_HOST, 'search')
+
         redirect = self.server.redirects.get(self.path)
         if redirect:
             sa = self.server.socket.getsockname()
@@ -82,6 +140,19 @@ class RequestHandler(SimpleHTTPRequestHandler):
             path += '/'
         return path
 
+def get_http_client_info(upstream_name, url):
+    """
+    return the http.client class and host needed to reach the given url
+    """
+    if not url:
+        return None, None
+    scheme, host, *_ = urllib.parse.urlparse(url)
+    if not scheme:
+        print('Must include scheme in {}-proxy-url (e.g. https://example.com, rather than example.com)'.format(upstream_name))
+    Client = getattr(http.client, scheme.upper() + 'Connection')
+    print('PROXYING: "/_{}" to {}'.format(upstream_name, url))
+    return Client, host
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--bind', '-b', default='127.0.0.1', metavar='ADDRESS',
@@ -91,8 +162,12 @@ if __name__ == '__main__':
                         default=8000, type=int,
                         nargs='?',
                         help='Specify alternate port [default: 8000]')
-    args = parser.parse_args()
+    parser.add_argument('--search-proxy-url', default=DEFAULT_SEARCH_URL, metavar='SEARCH_URL',
+                        help='url to proxy search requests to. [default: {}'.format(DEFAULT_SEARCH_URL))
+    parser.add_argument('--portal-proxy-url', default=None, metavar='PORTAL_URL',
+                        help='url to proxy portal requests to. [default: None]')
 
+    args = parser.parse_args()
     server_address = (args.bind, args.port)
 
     httpd = HTTPServer(server_address, RequestHandler)
@@ -108,6 +183,11 @@ if __name__ == '__main__':
 
     sa = httpd.socket.getsockname()
     print("Serving HTTP on", sa[0], "port", sa[1], "...")
+    print("\n\n*** This server is designed for local use. DO NOT USE IN PRODUCTION ***\n\n")
+    PORTAL_CLIENT_CLASS, PORTAL_HOST = get_http_client_info('portal', args.portal_proxy_url)
+
+    SEARCH_CLIENT_CLASS, SEARCH_HOST = get_http_client_info('search', args.search_proxy_url)
+
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
